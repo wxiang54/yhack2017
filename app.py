cce1b502359c08c5d906b99a52417212e0c2d087
will <willxiang1999@gmail.com>
2017-12-03 03:41:10 -0500

    return

--- app.py
+++ app.py
@@ -6,18 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/update/')
 def update():
+    return
 
 @app.route('/add/')
 def add():
+    return
 
 @app.route('/list/')
 def list():
+    return
 
 @app.route('/items/')
 def items():
+    return
 
 @app.route('/manage/')
 def manage():
+    return
 
 if __name__ == '__main__':
     app.run()
