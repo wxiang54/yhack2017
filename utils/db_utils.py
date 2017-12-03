0418af4296cc560cc7dd6e91a23ace7464f50d21
will <willxiang1999@gmail.com>
2017-12-03 06:35:20 -0500

    bet

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -2,21 +2,28 @@ import sqlite3
 from sqlite3 import Error
 import os.path
 
+
 PATH_TO_DB = "data/data.db"
 db = sqlite3.connect(PATH_TO_DB)
 c = db.cursor()
-if os.path.isfile("data.db"):
-    setup_db()
-    
+
+
 def setup_db():
+    q = '''
+CREATE TABLE ItemType (
+    id integer NOT NULL CONSTRAINT ItemType_pk PRIMARY KEY,
+    name varchar(50),
+    est_fin_days double,
+    catagories_id integer NOT NULL
+);
+'''
+    c.execute(q)
     q = '''
 CREATE TABLE Item (
    id integer NOT NULL CONSTRAINT Item_pk PRIMARY KEY,
-   name varchar(50),
-   exp_date datetime NOT NULL,
-   qty integer,
-   est_fin_days double,
-   catagories_id integer NOT NULL
+   itemtype_id integer NOT NULL,
+   exp_date date NOT NULL,
+   qty integer
 );
 '''
     c.execute(q)
@@ -35,7 +42,8 @@ CREATE TABLE ListItem (
 );
 '''
     c.execute(q)
-    q = ''' CREATE TABLE Catagory (
+    q = ''' 
+CREATE TABLE Catagory (
     id integer NOT NULL CONSTRAINT Catagories_pk PRIMARY KEY,
     name character(50)
 );
@@ -43,25 +51,53 @@ CREATE TABLE ListItem (
     c.execute(q)
     db.commit()
 
-def addItem(catagory, item):
-    q = "INSERT INTO Item VALUES (?, ?);"
-    c.execute(q, (getCatagoryId(catagory), getItemId(item)))
+    
+def addItem(itemName, exp_date, qty):
+    q = "INSERT INTO Item(itemtype_id, exp_date, qty) VALUES (?, ?, ?);"
+    c.execute(q, (getItemTypeID(itemName), exp_date, qty))
+    db.commit()
+
+#add item type
+def createItem(catagoryName, itemName):
+    q = "INSERT INTO ItemType(name, catagories_id) VALUES (?, ?);"
+    c.execute(q, (itemName, getCatagoryID(catagoryName)))
+    db.commit()
+
+def createCatagory(name):
+    q = "INSERT INTO Catagory(name) VALUES (?);"
+    c.execute(q, (name,))
     db.commit()
     
 def getCatagoryID(name):
-    q = "SELECT id FROM Catagory WHERE name='?';"
-    return c.execute(q, (name)).fetchone()[0]
+    q = "SELECT id FROM Catagory WHERE name='%s';" % name
+    return c.execute(q).fetchone()[0]
 
-def getItemID(name):
-    q = "SELECT id FROM Item WHERE name='?';"
-    return c.execute(q, (name)).fetchone()[0]
+def getItemTypeID(name):
+    q = "SELECT id FROM ItemType WHERE name='%s';" % name
+    return c.execute(q).fetchone()[0]
 
 #gets names only
 def getAllCatagories():
-    q = "SELECT name FROM Catagory;"
+    q = "SELECT name FROM Catagory ORDER BY name ASC;"
     return c.execute(q).fetchall()
 
 def getAllItems():
-    q = "SELECT name FROM Item;"
+    q = "SELECT name FROM Item ORDER BY name ASC;"
     return c.execute(q).fetchall()
 
+if __name__ == "__main__":
+
+    if c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Item';").fetchone():
+        print 'db existed'
+    else:
+        setup_db()
+        createCatagory("Food")
+        createCatagory("Medicine")
+        createItem("Food", "Lettuce")
+        createItem("Food", "Tomato")
+        createItem("Medicine", "Xans")
+        createItem("Medicine", "Mol")
+        addItem("Tomato", "2017-12-03", 5)
+        addItem("Xans", "2018-02-02", 2)
+    
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
