e26f2f5d8e943b7215bf4a7567bded21642019c1
will <willxiang1999@gmail.com>
2017-12-03 03:55:31 -0500

    ay

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,19 +1,12 @@
 import sqlite3
 from sqlite3 import Error
- 
- 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
+import os.path
 
+PATH_TO_DB = "data/data.db"
 
 def setup_db():
+    db = sqlite3.connect(PATH_TO_DB)
+    c = db.cursor()
     q = '''
 CREATE TABLE Item (
    id integer NOT NULL CONSTRAINT Item_pk PRIMARY KEY,
@@ -23,23 +16,32 @@ CREATE TABLE Item (
    est_fin_days double,
    catagories_id integer NOT NULL
 );
-
+'''
+    c.execute(q)
+    q = '''
 CREATE TABLE List (
    id integer NOT NULL CONSTRAINT List_pk PRIMARY KEY,
    name varchar(50)
 );
-
+'''
+    c.execute(q)
+    q = '''
 CREATE TABLE ListItem (
    id integer NOT NULL CONSTRAINT ListItem_pk PRIMARY KEY,
    list_id integer NOT NULL,
    item_id integer NOT NULL
 );
-
-CREATE TABLE Catagories (
-   id integer NOT NULL CONSTRAINT Catagories_pk PRIMARY KEY,
-   name character(50)
+'''
+    c.execute(q)
+    q = ''' CREATE TABLE Catagories (
+    id integer NOT NULL CONSTRAINT Catagories_pk PRIMARY KEY,
+    name character(50)
 );
-    '''
-        
+'''
+    c.execute(q)
+    db.commit()
+
+    
 if __name__ == '__main__':
-    create_connection("items.db")
+    if not os.path.isfile("data.db"):
+        setup_db()
